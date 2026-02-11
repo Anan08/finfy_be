04a7f9d644c745555038f1dc74ebb2472b11c7ba
Anan08 <anandapgt1@gmail.com>
2026-02-11 20:52:23 +0700

    fix : update transaction

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -11,40 +11,40 @@ exports.addTransaction = async (req, res) => {
         const dateOfTransaction = date ? new Date(date) : new Date();
 
         if (!description || !amount) {
-            return res.status(400).json({message: 'Please provide all required fields'});
+            return res.status(400).json({ message: 'Please provide all required fields' });
         }
 
         const user = await User.findById(req.user.id);
-        
+
         if (!user) {
-            return res.status(400).json({message: 'User not found'});
+            return res.status(400).json({ message: 'User not found' });
         }
 
         const categoryExists = await Category.findOne({
-            _id : category
+            _id: category
         })
 
         const transactionTypeExists = await TransactionType.findOne({
-            _id : transactionType
+            _id: transactionType
         })
 
         if (!categoryExists) {
-            return res.status(400).json({message: 'Category not found'});
+            return res.status(400).json({ message: 'Category not found' });
         }
 
         const transaction = new Transaction({
             ...req.body,
-            date : dateOfTransaction,
-            category : categoryExists._id,
-            type : transactionTypeExists._id,
-            userId : req.user.id
+            date: dateOfTransaction,
+            category: categoryExists._id,
+            type: transactionTypeExists._id,
+            userId: req.user.id
         })
 
         await transaction.save();
-        return res.status(201).json({message : 'Transaction added successfully', transaction : transaction});
+        return res.status(201).json({ message: 'Transaction added successfully', transaction: transaction });
     } catch (error) {
         console.log(error);
-        return res.status(400).json({error : error.message})
+        return res.status(400).json({ error: error.message })
     }
 }
 
@@ -66,38 +66,42 @@ exports.getTransactions = async (req, res) => {
 exports.deleteTransaction = async (req, res) => {
     try {
         const id = req.params.id;
-        
+
         const transaction = await Transaction.findOneAndDelete({
-            _id : id,
-            userId : req.user.id
+            _id: id,
+            userId: req.user.id
         });
 
         if (!transaction) return res.status(404).json({ error: 'Transaction not found' });
 
-        return res.status(200).json({message : 'Transaction deleted successfully'});
+        return res.status(200).json({ message: 'Transaction deleted successfully' });
     } catch (error) {
         console.log(error);
-        return res.status(400).json({error : error.message})
+        return res.status(400).json({ error: error.message })
     }
 }
 
 exports.updateTransaction = async (req, res) => {
     try {
         const id = req.params.id;
-        const { description, amount, category, date } = req.body;
+        const { description, amount, category, date, transactionType } = req.body;
+        const updateData = { description, amount, category, date };
+
+        if (transactionType) {
+            updateData.type = transactionType;
+        }
+
         const tranasction = await Transaction.findOneAndUpdate({
-            _id : id,
-            userId : req.user.id
-        }, {
-            description, amount, category, date
-        }, { new : true });
+            _id: id,
+            userId: req.user.id
+        }, updateData, { new: true });
 
         if (!tranasction) return res.status(404).json({ error: 'Transaction not found' });
 
-        return res.status(200).json({message : 'Transaction updated successfully', transaction : tranasction});
+        return res.status(200).json({ message: 'Transaction updated successfully', transaction: tranasction });
     } catch (error) {
         console.log(error);
-        return res.status(400).json({error : error.message})
+        return res.status(400).json({ error: error.message })
     }
 }
 
